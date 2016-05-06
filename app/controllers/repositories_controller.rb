8b3e44c3e3193d38fc5411a61f0d2828a6f3e0e3
authorbeard <msnolan@gmail.com>
2016-05-06 13:49:31 -0400

    all specs passing with rspec refactor

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,21 @@
 class RepositoriesController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
+  # byebug
+    gh=GithubService.new({"access_token"=> session[:token]})
+    @user = gh.get_username
+    @repos_array = gh.get_repos
+
+    # response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    # @repos_array = JSON.parse(response.body)
   end
 
   def create
-    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    gh=GithubService.new({"access_token"=>session[:token]})
+    gh.create_repo(params[:name])
+
+    # response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     redirect_to '/'
   end
 end
